fdedbea3771766df162b55db3792bbdebaae0dff
Miguel Angel <miguel.delta21@gmail.com>
2025-02-21 00:02:16 -0500

    Start to develop about me section

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -19,7 +19,7 @@ function MainSection() {
 
   return (
     <div className="flex flex-row lg:absolute justify-center items-center text-center w-auto h-auto lg:w-[55vw] lg:h-[60vh] px-14 gap-x-5 drop-shadow-2xl mt-10 lg:top-[40%] lg:left-[28%] lg:-translate-x-1/2 lg:-translate-y-1/2">
-      <div className="sm:text-[4vw] max-sm:text-[5.5vw] lg:w-full w-[65vw] max-sm:h-[20vh] max-sm:text-center animate-slide-in-blurred-top text-center items-center ">
+      <div className="sm:text-[4vw] max-sm:text-[7.5vw] lg:w-full w-[65vw] max-sm:h-[20vh] max-sm:text-center animate-slide-in-blurred-top text-center items-center max-sm:mb-10">
         <span
           onClick={displayGif}
           className="text-gray-300 text-shadow font-bold"
@@ -50,7 +50,7 @@ function MainSection() {
         </span>
 
         <div className="grid grid-cols-2 absolute left-1/2 -translate-x-1/2 -bottom-36 w-52 h-32 items-center place-items-center">
-          <div className="flex text-gray-300 text-shadow font-bold bg-white  w-12 h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300">
+          <div className="flex text-gray-300 text-shadow font-bold bg-white  w-12 h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300 max-sm:w-8 max-sm:h-8">
             <a
               href="https://github.com/DevilsWillCry"
               target="_blank"
@@ -65,7 +65,7 @@ function MainSection() {
             </a>
           </div>
 
-          <div className="flex text-gray-300 text-shadow font-bold bg-white  w-12 h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300">
+          <div className="flex text-gray-300 text-shadow font-bold bg-white  w-12 h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300 max-sm:w-8 max-sm:h-8">
             <a
               href="https://www.linkedin.com/in/miguel-angel-me%C3%B1aca-coral-037b06213/"
               target="_blank"
@@ -80,7 +80,7 @@ function MainSection() {
             </a>
           </div>
 
-          <div className="col-span-full flex text-gray-300 text-shadow font-bold bg-white w-[75%] h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300">
+          <div className="col-span-full flex text-gray-300 text-shadow font-bold bg-white w-[75%] h-12 items-center justify-center rounded hover:scale-125 hover:bg-gray-500 hover:shadow-2xl cursor-pointer transition-all duration-300 max-sm:w-[65%] max-sm:h-8 ">
             <a
               href="https://github.com/miguelangelgarcia"
               target="_blank"
